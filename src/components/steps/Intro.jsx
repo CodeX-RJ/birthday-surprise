c214d969c8327cfb695c451149fc22741f34d367
RJ <ramakantjai91@gmail.com>
2025-12-31 20:07:24 +0530

    new

--- src/components/steps/Intro.jsx
+++ src/components/steps/Intro.jsx
@@ -3,6 +3,35 @@ import Step from '../ui/Step'
 export default function Intro({ onNext }) {
   return (
     <Step>
+      {/* New Year Message */}
+      <h1>Happy New Year</h1>
+
+      <p>
+        A new year begins today.
+        <br />
+        A quiet moment of change.
+        <br />
+        A fresh start.
+      </p>
+
+      <p>
+        I hope this year brings you
+        <br />
+        peace where you need it,
+        <br />
+        strength where you don’t expect it,
+        <br />
+        and moments that feel right to you.
+      </p>
+
+      <p>
+        And since this new beginning
+        <br />
+        also happens to be something special
+        <br />
+      </p>
+
+      {/* Your original intro */}
       <h1>Before we begin…</h1>
 
       <p>
